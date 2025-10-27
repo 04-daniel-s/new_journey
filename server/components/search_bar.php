013cb2d3510a2b820499b8c5a627abe89e47823a
04-daniel-s <xininfected@gmail.com>
2025-10-27 21:12:53 +0100

    impl search bar

---
+++ server/components/search_bar.php
@@ -0,0 +1,5 @@
+<div class="input-group mb-3 py-5 bg-light">
+    <label class="input-group-text">></label>
+    <input class="p-2 form-control" type="text" name="search-loc" placeholder="Land der Reise">
+    <button type="button" class="btn btn-outline-secondary">Suchen</button>
+</div>
--- server/homepage.php
+++ server/homepage.php
@@ -1,8 +1,15 @@
-<div class="hero-image position-relative">
-    <div class="d-lg-none position-absolute top-50 start-50 translate-middle">
-        <h1 style="letter-spacing: 0.3rem; text-shadow: 0 4px 24px rgba(0,0,0,0.5)" class="text-white fw-bold display-1 text-uppercase text-center">Unterkunft buchen</h1>
+<div>
+
+    <div class="hero-image position-relative d-flex justify-content-end">
+        <div class="d-lg-none position-absolute top-50 start-50 translate-middle">
+            <h1 style="letter-spacing: 0.3rem; text-shadow: 0 4px 24px rgba(0,0,0,0.5)"
+                class="text-white fw-bold display-1 text-uppercase text-center">Unterkunft buchen</h1>
+        </div>
+
     </div>
-    <div>
-        <h5>Finde eine passende Unterkunft für deine nächste Reise</h5>
+
+    <div class="container-fluid d-flex justify-content-center">
+        <?php include 'components/search_bar.php'; ?>
     </div>
+
 </div>
